fe658b37385522fcc3433e3b348dab503173b3e8
andriawan24 <fawaznaufal23@gmail.com>
2026-04-09 14:25:15 +0700

    fix: Theme provider type props not suitable with children property

--- packages/ui/src/components/theme-provider.tsx
+++ packages/ui/src/components/theme-provider.tsx
@@ -1,7 +1,8 @@
-"use client"
+'use client';
 
-import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes"
+import type { PropsWithChildren } from 'react';
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from 'next-themes';
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function ThemeProvider({ children, ...props }: PropsWithChildren<ThemeProviderProps>) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
